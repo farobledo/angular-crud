6f2a2acbc78f4b6f93830756c45e0c787cf0b14e
Fabian Andres Robledo Olave <83982603+farobledo@users.noreply.github.com>
2021-12-28 16:34:10 -0500

    Crud Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+import {Empleados} from './models/empleados';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crud';
+  empleadosArray: Empleados[] = [
+    {id:1, nombre: "Jose", pais: "Chile"},
+    {id:2, nombre: "Carlos", pais: "Colombia"},
+    {id:3, nombre: "Camilo", pais: "Argentina"},
+  ];
+ 
+  selectedEmpleados: Empleados = new Empleados();
+  openForEditar(empleados: Empleados){
+     this.selectedEmpleados = empleados;
+  }
+
+  addOrEditar(){
+    this.selectedEmpleados.id = this.empleadosArray.length + 1;
+    this.empleadosArray.push(this.selectedEmpleados);
+
+    this.selectedEmpleados = new Empleados();
+  }
+
+  delete(){
+  if(confirm('Quieres Eliminar tus Datos')){
+    this.empleadosArray = this.empleadosArray.filter(x => x != this.selectedEmpleados);
+    this.selectedEmpleados = new Empleados();
+  }
+
+  }
+
 }
